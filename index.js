89dc25afc3e74c3e322673c2dcbc2e2f2227f974
N1haal_reddy <knihalreddy5@gmail.com>
2026-02-12 16:12:11 +0530

    Add dice rolling functionality with images

---
+++ index.js
@@ -0,0 +1,21 @@
+
+rolldice();
+function rolldice(){
+    var randomnumber = Math.floor(Math.random()*6) + 1;
+    var image1 = "images/dice" + randomnumber + ".png";
+    document.querySelector(".img1").setAttribute("src", image1);
+    
+    var randomnumber2 = Math.floor(Math.random()*6) + 1;     
+    var image2 = "images/dice" + randomnumber2 + ".png";
+    document.querySelector(".img2").setAttribute("src", image2);
+    if (randomnumber > randomnumber2){
+        document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    }
+    else if (randomnumber2 > randomnumber){
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+}
+document.querySelector("#btn").addEventListener("click", rolldice);
