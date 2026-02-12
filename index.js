3ba49a8b726c70642fa113f07fd2afb6ea8330f6
N1haal_reddy <knihalreddy5@gmail.com>
2026-02-12 16:24:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 rolldice();
 function rolldice(){
     var randomnumber = Math.floor(Math.random()*6) + 1;
-    var image1 = "images/dice" + randomnumber + ".png";
+    var image1 = "dice" + randomnumber + ".png";
     document.querySelector(".img1").setAttribute("src", image1);
     
     var randomnumber2 = Math.floor(Math.random()*6) + 1;     
-    var image2 = "images/dice" + randomnumber2 + ".png";
+    var image2 = "dice" + randomnumber2 + ".png";
     document.querySelector(".img2").setAttribute("src", image2);
     if (randomnumber > randomnumber2){
         document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
